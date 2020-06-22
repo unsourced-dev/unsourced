5d48d81bf825c035d225019b59d4b57ea8a71d56
Anthony Ferrando <ferrando.tony@gmail.com>
2020-06-22 12:28:05 +0200

    Add caption to Image

--- src/ui/Image.tsx
+++ src/ui/Image.tsx
@@ -12,10 +12,11 @@ export interface ImageProps {
   src: string
   alt?: string
   className?: string
+  caption?: string
 }
 
 export function Image(props: ImageProps) {
-  const { height, width, src, alt } = props
+  const { height, width, src, alt, caption } = props
   const className = cc(
     getUnresponsiveFromPropName(props, "objectFit"),
     getUnresponsiveFromPropName(props, "borderRadius"),
@@ -23,6 +24,15 @@ export function Image(props: ImageProps) {
   )
 
   if (!src) return <div />
+
+  if (caption) {
+    return (
+      <figure>
+        <img className={className} src={src} alt={alt} />
+        {caption && <figcaption className="my-2 text-center text-gray-700">{caption}</figcaption>}
+      </figure>
+    )
+  }
 
   return <img className={className} src={src} alt={alt} height={height} width={width} style={{ height: "inherit" }} />
 }
